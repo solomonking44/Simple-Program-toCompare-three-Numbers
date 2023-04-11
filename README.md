843d3d719baf011209f501cafea8380b58ff28e6
solomonking44 <mugishasolomon520@gmail.com>
2023-04-11 09:13:51 +0300

    Compares Three numbers

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple program checks 3 numbers for largest one 
+
+> Also checks whether 2 or 3 numbers are equal
---
+++ app.py
@@ -0,0 +1,35 @@
+def compare(a, b, c):
+    if a == b == c:
+        print(f"All numbers are equal therefore {a} is the largest")
+    else:
+        if a > b and a != b:
+            if a > c and a != c:
+                print(f"{a} -> a is the largest")
+            elif c > a and c != a:
+                print(f"{c} -> c is the largest")
+            else:
+                print(f"{c} -> a and c are equal")
+        else:
+            if a == b:
+                print(f"{a} -> a and b are equal")      
+            elif b > c and b != c:
+                print(f"{b} -> b is the largest")
+            elif c > b and c != b:
+                print(f"{c} -> c is the largest")
+            elif b==c:
+                print(f"{c} -> b and c are equal")
+            
+    
+
+
+print("Enter A")
+a = input()
+print("Enter B")
+b = input()
+print("Enter C")
+c = input()
+
+print(f"a = {a}, b = {b} and c = {c}")
+
+
+compare(a, b, c)
